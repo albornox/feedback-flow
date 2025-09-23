a70011dced9d16f0c90d41f95b9942a63c164a32
albornox <78057751+albornox@users.noreply.github.com>
2025-09-23 18:26:55 -0500

    actualizado2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "Feedback Flow funcionando!";
-echo "<br><a href='/d/'>Dashboard</a>";
+echo "<br><a href='/dashboard-admin/'>Dashboard</a>";
 echo "<br><a href='/setup-db.php'>Setup BD</a>";
 ?>
