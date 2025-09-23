2fa978ab077fdb19ef4161b35b82ba6e3666c817
albornox <78057751+albornox@users.noreply.github.com>
2025-09-23 18:16:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "Feedback Flow funcionando!";
-echo "<br><a href='/dashboard/'>Dashboard</a>";
+echo "<br><a href='/d/'>Dashboard</a>";
 echo "<br><a href='/setup-db.php'>Setup BD</a>";
 ?>
